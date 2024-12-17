671bf4ec68f14aeb3abde63bafc7b197d5813aab
Asmit Gawande <asmitgawande1307@gmail.com>
2024-12-18 00:01:10 +0530

    this is now important

--- frontend-Travel-system/src/components/BookingForm.jsx
+++ frontend-Travel-system/src/components/BookingForm.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { createBooking } from '../api';
 
 const BookingForm = () => {
   const location = useLocation();
